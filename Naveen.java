7357ee0fbc0d9f8974d94b8596701afe4d0a7156
pudupeti naveen <167106262+pudupetinaveen@users.noreply.github.com>
2024-06-06 19:55:25 +0530

    Add files via upload

---
+++ Naveen.java
@@ -0,0 +1,20 @@
+class Naveen{
+	public void IfCondition(){
+		String str1="Apple";
+		String str2="Banana";
+		
+		if(str1!=str2){
+			System.out.println("If condition is True");
+	}
+	
+		else{
+		System.out.println("If condition is false");
+		}
+	}
+public static void main(String[] args){
+	Naveen nobj=new Naveen();
+	nobj.IfCondition();
+
+
+}
+}
---
+++ S.txt
@@ -0,0 +1,15 @@
+class Inheritance{
+	public void m1(){
+	int a=10;
+	System.out.println("SingleInheritance"+a);
+	} 
+	
+	class SingleInheritance extends Inheritance{
+		public void m2(){
+			int b=20;
+			System.out.println("SingleInheritance"+b);
+		}
+	public static void main(String[] args){
+	}
+}
+
